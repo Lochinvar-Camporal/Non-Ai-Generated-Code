c2fd468b629268c13438c1b6a917deb1f0aea57c
Lochinvar-Camporal <OrangeBlue47@proton.me>
2025-07-30 16:17:36 -0400

    Create is_even_fn.rs

---
+++ is_even_fn.rs
@@ -0,0 +1,9 @@
+// write a function named is_even that: takes a i32 returns true if the number is even, and false if not
+fn is_even(x: i32) -> bool { 
+    let myMod = x % 2; 
+    if ( myMod == 0 ) { 
+            true } 
+        else { 
+            false } 
+
+} // is this good
